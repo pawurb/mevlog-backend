5f85d040c7fa92a72d9be09bfc8c0d3a987427b7
pawurb <contact@pawelurbanek.com>
2025-09-16 18:46:58 +0200

    Landing text

--- src/controllers/html/home_controller.rs
+++ src/controllers/html/home_controller.rs
@@ -9,7 +9,7 @@ use crate::{
 };
 
 // force html views recompilation by changing this value
-const _VIEW_VERSION: u64 = 9;
+const _VIEW_VERSION: u64 = 10;
 
 #[derive(Template)]
 #[template(path = "home.html")]
