26dc801e043c0eeb6db73b6ea2699fc5f5a2e737
pawurb <contact@pawelurbanek.com>
2025-10-20 22:12:24 +0200

    Update landing

--- src/controllers/html/home_controller.rs
+++ src/controllers/html/home_controller.rs
@@ -8,7 +8,7 @@ use crate::{
 };
 
 // force html views recompilation by changing this value
-const _VIEW_VERSION: u64 = 10;
+const _VIEW_VERSION: u64 = 11;
 
 #[derive(Template)]
 #[template(path = "home.html")]
