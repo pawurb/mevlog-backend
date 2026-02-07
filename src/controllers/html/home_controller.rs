a3cade0b5245d231c82f9c4a06b45646f78e794c
pawurb <contact@pawelurbanek.com>
2026-02-07 22:56:31 +0100

    Link hotpath

--- src/controllers/html/home_controller.rs
+++ src/controllers/html/home_controller.rs
@@ -8,7 +8,7 @@ use crate::{
 };
 
 // force html views recompilation by changing this value
-const _VIEW_VERSION: u64 = 16;
+const _VIEW_VERSION: u64 = 17;
 
 #[derive(Template)]
 #[template(path = "home.html")]
